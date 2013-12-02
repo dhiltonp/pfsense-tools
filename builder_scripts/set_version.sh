dhiltonp/pfsense-tools

commit cda487dd0fc41d9d4fb51a8504b9893b7e752061

set_version.sh checks for USE_SVNUP

builder_scripts/set_version.sh
@@ -47,11 +47,20 @@ END_OF_USAGE
 	exit 127
 fi
 
+# Source pfsense-build.conf variables
+. ./pfsense_local.sh
+. ./pfsense-build.conf
+
 # Default SUPHOST
 if [ "$2" != "" ]; then 
 	SUPHOST="$2"
 else 
-	if [ -f /usr/local/bin/fastest_cvsup ]; then
+	if [ -n "${USE_SVNUP}" ]; then 
+		echo "WARNING: If /usr/local/etc/svnup.conf is not configured, svnup will fail!"
+		echo 
+		echo "Either set a server in /usr/local/etc/svnup.conf or pass in a svn server as the second parameter"
+		sleep 2
+	elif [ -f /usr/local/bin/fastest_cvsup ]; then
 		echo "One moment please, finding the best cvsup server to use..."
 		SUPHOST=`fastest_cvsup -c tld -q`
 	else 
@@ -94,10 +103,6 @@ touch pfsense-build.conf
 export BUILDER_SCRIPTS=`pwd`
 export BUILDER_TOOLS=${BUILDER_SCRIPTS}/..
 export BASE_DIR=${BUILDER_TOOLS}/..
-
-# Source pfsense-build.conf variables
-. ./pfsense_local.sh
-. ./pfsense-build.conf
 
 strip_pfsense_local() {
 	# Strip dynamic values

commit f3fa1d498f778ce93224fdf78146c80d786d8161

set_version.sh: don't break if pfsense-build.conf doesn't yet exist (regression in cda487dd0fc41d9d4fb51a8504b9893b7e752061)

builder_scripts/set_version.sh
@@ -47,9 +47,15 @@ END_OF_USAGE
 	exit 127
 fi
 
-# Source pfsense-build.conf variables
+# reset pfsense-build.conf
+rm -f pfsense-build.conf
+
+export BUILDER_SCRIPTS=`pwd`
+export BUILDER_TOOLS=${BUILDER_SCRIPTS}/..
+export BASE_DIR=${BUILDER_TOOLS}/..
+
+# import any pfsense_local vars (USE_SVNUP is used; idk about other vars)
 . ./pfsense_local.sh
-. ./pfsense-build.conf
 
 # Default SUPHOST
 if [ "$2" != "" ]; then 
@@ -95,14 +101,6 @@ if [ "$7" != "" ]; then
 fi
 
 HANDLED=false
-
-# Ensure file exists
-rm -f pfsense-build.conf
-touch pfsense-build.conf
-
-export BUILDER_SCRIPTS=`pwd`
-export BUILDER_TOOLS=${BUILDER_SCRIPTS}/..
-export BASE_DIR=${BUILDER_TOOLS}/..
 
 strip_pfsense_local() {
 	# Strip dynamic values